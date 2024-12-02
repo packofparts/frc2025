85c8e88b0787f231cd44abf0a3d69930fc7f9c76
Daniel Gorbatov <dzgorbatov@gmail.com>
2024-12-02 15:55:59 -0800

    swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,6 @@ import frc.robot.subsystems.Swerve;
 import POPLib.Controllers.OI.OI;
 import POPLib.Controllers.OI.XboxOI;
 import POPLib.Swerve.Commands.TeleopSwerveDrive;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
