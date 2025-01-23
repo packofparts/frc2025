1aa9d36764fcf84beceac4e9b9e3d1778940a778
SaranyaA20 <146662808+SaranyaA20@users.noreply.github.com>
2025-01-22 20:36:51 -0800

    implemented moveToPose, moveToNearestScoringPos, and moveToAprilTag based on april tag data

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -3,6 +3,10 @@ package frc.robot.subsystems;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+
+import org.photonvision.EstimatedRobotPose;
+
+import poplib.sensors.camera.Camera;
 import poplib.sensors.camera.CameraConfig;
 import poplib.sensors.camera.Limelight;
 import poplib.sensors.camera.LimelightConfig;
@@ -13,6 +17,7 @@ import poplib.swerve.swerve_modules.SwerveModuleTalon;
 import poplib.swerve.swerve_templates.VisionBaseSwerve;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -28,6 +33,7 @@ public class Swerve extends VisionBaseSwerve {
     private static Swerve instance;
     private static List<CameraConfig> cameraConfigs;
     private static List<LimelightConfig> limelightConfigs;
+    private static Camera camera;
 
     
     public static Swerve getInstance() {
@@ -62,7 +68,7 @@ public class Swerve extends VisionBaseSwerve {
      * poseSupplier = reference to april tag position
      * newOffset = vector relative to poseSupplier/where the robot needs to be
     */
-    private Command moveToPose(Supplier<Pose2d> poseSupplier, Translation2d newOffset) {
+    private Command moveToPoseOdom(Supplier<Pose2d> poseSupplier, Translation2d newOffset) {
         PIDController yaxisPid = Constants.AutoAlign.Y_PID_CONTROLLER;
         PIDController xaxisPid = Constants.AutoAlign.X_PID_CONTROLLER;
         PIDController thetaPid = Constants.AutoAlign.THETA_PID_CONTROLLER;
@@ -130,6 +136,74 @@ public class Swerve extends VisionBaseSwerve {
         );
     }
 
+    private Command moveToPoseVision(Supplier<Pose2d> poseSupplier, Translation2d newOffset) {
+        PIDController yaxisPid = Constants.AutoAlign.Y_PID_CONTROLLER;
+        PIDController xaxisPid = Constants.AutoAlign.X_PID_CONTROLLER;
+        PIDController thetaPid = Constants.AutoAlign.THETA_PID_CONTROLLER;
+        
+        thetaPid.enableContinuousInput(0, 2 * Math.PI);
+
+        xaxisPid.setTolerance(Constants.AutoAlign.X_TOLERANCE);
+        yaxisPid.setTolerance(Constants.AutoAlign.Y_TOLERANCE);
+        thetaPid.setTolerance(Constants.AutoAlign.THETA_TOLERANCE);
+
+
+        return runOnce(() -> {
+
+            Translation2d offset = newOffset;
+
+            if (offset == null) {
+                offset = Constants.AutoAlign.DEFAULT_OFFSET;
+            }
+
+            /** Get forward vector of pose */
+            Pose2d pose = poseSupplier.get();
+            Rotation2d targetRot = pose.getRotation();
+            
+            /** Add robot to pose vector to the offset vector */
+            offset = offset.rotateBy(targetRot);
+            Translation2d offsetTarget = pose.getTranslation().plus(offset); 
+
+            super.field.getObject("target").setPose(new Pose2d(offsetTarget, targetRot));
+                   
+            /**  Set pid setpoints */
+            xaxisPid.setSetpoint(offsetTarget.getX());
+            yaxisPid.setSetpoint(offsetTarget.getY());
+
+            /** Invert theta to ensure we're facing towards the target */
+            thetaPid.setSetpoint(targetRot.minus(Constants.AutoAlign.DEFAULT_ROTATION).getRadians());
+
+            xaxisPid.calculate(camera.getEstimatedPose(pose).get().estimatedPose.getX());
+            yaxisPid.calculate(camera.getEstimatedPose(pose).get().estimatedPose.getY());
+            thetaPid.calculate(camera.getEstimatedPose(pose).get().estimatedPose.getRotation().getAngle());
+
+        }).andThen(run(
+            () -> {
+                Alliance color;
+                if (AllianceColor.getInstance().isRed() == true) {
+                    color = Alliance.Red;
+                }
+                else {
+                    color = Alliance.Blue;
+                }
+                drive(
+                    new Translation2d(
+                        xaxisPid.calculate(camera.getEstimatedPose(poseSupplier.get()).get().estimatedPose.getX()),
+                        yaxisPid.calculate(camera.getEstimatedPose(poseSupplier.get()).get().estimatedPose.getY())),
+                    thetaPid.calculate(camera.getEstimatedPose(poseSupplier.get()).get().estimatedPose.getRotation().getAngle()),
+                    color);
+            }
+        )).until(
+            () -> xaxisPid.atSetpoint() && yaxisPid.atSetpoint() && thetaPid.atSetpoint()
+        ).andThen(
+            () -> { 
+                xaxisPid.close(); 
+                yaxisPid.close(); 
+                thetaPid.close(); 
+            }
+        );
+    }
+
     private Pose2d getNearestScoringPos() {
         Translation2d currentTranslation = odom
             .getEstimatedPosition()
@@ -151,21 +225,30 @@ public class Swerve extends VisionBaseSwerve {
         return closestScoringPos; 
     }
 
-    public Command moveToNearestScoringPos(Translation2d tagOffset) {
-        return moveToPose(() -> getNearestScoringPos(), tagOffset);
+    public Command moveToNearestScoringPosOdom(Translation2d tagOffset) {
+        return moveToPoseOdom(() -> getNearestScoringPos(), tagOffset);
+    }
+
+    public Command moveToNearestScoringPosVision(Translation2d tagOffset) {
+        return moveToPoseVision(() -> getNearestScoringPos(), tagOffset);
     }
 
     /**
      * Auto move to april tag.
      */
-    public Command moveToAprilTag(int tagID, Translation2d tagOffset) {
-        return moveToPose(
+    public Command moveToAprilTagOdom(int tagID, Translation2d tagOffset) {
+        return moveToPoseOdom(
             () -> Constants.Vision.APRIL_TAG_FIELD_LAYOUT.getTagPose(tagID).get().toPose2d(),
             tagOffset
         );
-    }
-
+    }  
     
+    public Command moveToAprilTagVision(int tagID, Translation2d tagOffset) {
+        return moveToPoseVision(
+            () -> Constants.Vision.APRIL_TAG_FIELD_LAYOUT.getTagPose(tagID).get().toPose2d(),
+            tagOffset
+        );
+    } 
 
 
     
