e5e12a8c1aba13765d3dee6693bdc51ed142981f
Daniel Gorbatov <dzgorbatov@gmail.com>
2025-01-31 12:30:30 -0800

    Alpha bot (#39)
    * working elevator
    
    * working alpha bot
    
    * fixed persistmode
    
    * clean up
    
    * add state machine and clean up
    
    * Try to fix the 'grab the latest version of poplib' functionality (#42)
    
    * working alpha
    
    ---------
    
    Co-authored-by: Martin Taillefer <geeknoid@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import poplib.control.FFConfig;
 import poplib.control.PIDConfig;
+import poplib.motor.ConversionConfig;
 import poplib.motor.FollowerConfig;
 import poplib.motor.Mode;
 import poplib.motor.MotorConfig;
@@ -20,6 +21,8 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+
+import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
@@ -27,6 +30,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.units.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -38,20 +42,15 @@ import edu.wpi.first.wpilibj.Filesystem;
  */
 public final class Constants {
     public static class Ports {
-        public static final String CANIVORE_NAME = "rio";
+        public static final String CANIVORE_NAME = "tempura sushi";
     }
 
     public static class Elevator {
-
-        // 150 L1
-        // 200 L2
-        // 350 L3
-        
-        enum SETPOINTS {
-            IDLE(0),     // this is my imaginary, untested setpoint for making the elevator go all the way down
-            L1(150),
-            L2(200),
-            L3(350);
+        public enum SETPOINTS {
+            IDLE(0),
+            L1(30),
+            L2(40),
+            L3(60);
 
             private double setpoint;
 
@@ -64,24 +63,22 @@ public final class Constants {
             }
         }
 
-        public static final boolean TUNNING_MODE = true;
+        public static final boolean TUNNING_MODE = false;
 
         public static final MotorConfig RIGHT_MOTOR = new MotorConfig(
             26, 
             20, 
             false, 
-            new PIDConfig(0.07, 0, 0, 0),
+            new PIDConfig(0.12, 0, 0, 0), // 0.1
             Mode.COAST
         );
 
-        public static final FFConfig FF_CONFIG = new FFConfig(0.26);
-
         public static final FollowerConfig LEFT_MOTOR = new FollowerConfig(RIGHT_MOTOR, false, 25);
-        public static final double upperSetpoint = 1.0;
-        public static final double lowerSetpoint = 0.0;
+
+        public static final ElevatorFeedforward FF = new ElevatorFeedforward(0, 0.47, 0);
 
         public static final double MOTOR_SPEED = 0.5;
-        public static final double MAX_ERROR = 0.1;
+        public static final double MAX_ERROR = 1.0;
     }
 
     public static final class Manipulator {
@@ -92,7 +89,7 @@ public final class Constants {
             Mode.COAST
         );    
 
-        public static final BeamBreakConfig BEAM_BREAK = new BeamBreakConfig(6);
+        public static final BeamBreakConfig BEAM_BREAK = new BeamBreakConfig(6, true);
 
         public static final double SPEED = 0.9;
     }
@@ -101,21 +98,31 @@ public final class Constants {
         public static final MotorConfig MOTOR = new MotorConfig(
             23, 
             40, 
+            false,
+            Mode.COAST
+        );    
+
+        public static final MotorConfig MOTOR2 = new MotorConfig(
+            30, 
+            40, 
             true, 
             Mode.COAST
         );    
 
-        public static final double SPEED = 0.5;
+        public static final double SPEED = 0.8;
     }
 
     public static final class Intake {
-        // 20 for algea picjup, 35 for algea drop off
+        public static final double GEAR_RATIO = 157.5;
+
         public static final MotorConfig PIVOT = new MotorConfig(
             22, 
+            "",
             40, 
             true, 
             new PIDConfig(0.08),
-            Mode.COAST
+            Mode.COAST,
+            new ConversionConfig(GEAR_RATIO, Units.Degrees)
         );    
 
         public static final MotorConfig SPIN = new MotorConfig(
@@ -123,20 +130,21 @@ public final class Constants {
             40, 
             false, 
             Mode.COAST
-        );    
+        );
 
-        public static final double GEAR_RATIO = 25.0 * 2.1;
-        public static final boolean TUNING_MODE = true;
-        public static final FFConfig ff = new FFConfig(0.5, 0.0, 0.0);
-        public static final AbsoluteEncoderConfig ENCODER = new AbsoluteEncoderConfig(2, new Rotation2d(360), false);
-        public static final double MAX_ERROR = 0.1;
+        public static final boolean TUNING_MODE = false;
+
+        public static final FFConfig FF = new FFConfig(0.5, 0.0, 0.0);
+
+        public static final AbsoluteEncoderConfig ENCODER = new AbsoluteEncoderConfig(9, Rotation2d.fromDegrees(-48.0), true);
+        public static final double MAX_ERROR = 1.0;
         public static final double SPEED = 1.0;
 
-        enum SETPOINTS {  
-            IDLE(0),  // this is a guess
-            ALGAE_PICKUP(20),
+        public enum SETPOINTS {  
+            IDLE(90),
+            ALGAE_PICKUP(25),
             ALGAE_DROP(35),
-            CORAL_PICKUP(50); // this is a guess
+            CORAL_PICKUP(-30);
 
             private double setpoint;
 
@@ -168,6 +176,7 @@ public final class Constants {
         );
 
         public static final MotorConfig ANGLE_CONFIG = new MotorConfig(
+            Ports.CANIVORE_NAME,
             25,
             false, // Make true if we have a stroke
             PIDConfig.getPid(5.0), // TODO: retune
@@ -176,6 +185,7 @@ public final class Constants {
 
 
         public static final MotorConfig DRIVE_CONFIG = new MotorConfig(
+            Ports.CANIVORE_NAME,
             60,
             true,
             PIDConfig.getPid(0.01, 0.2), // Tuned 01/05/25 with a shit battery
@@ -184,7 +194,7 @@ public final class Constants {
 
         public static final SDSModules MODULE_TYPE = SDSModules.MK4i;
 
-        public static final boolean SWERVE_TUNING_MODE = true;
+        public static final boolean SWERVE_TUNING_MODE = false;
 
         public static final SwerveModuleConstants[] SWERVE_MODULE_CONSTANTS = SwerveModuleConstants.generateConstants(
             new Rotation2d[] {
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,12 +8,18 @@ import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Indexer;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Manipulator;
+import frc.robot.subsystems.Swerve;
 import poplib.controllers.oi.OI;
 import poplib.controllers.oi.XboxOI;
-import poplib.smart_dashboard.TunableNumber;
+import poplib.swerve.commands.TeleopSwerveDrive;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -22,101 +28,112 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // Swerve swerve;
-  Elevator elevator;
-  Indexer indexer;
-  Intake intake;
-  Manipulator manipulator;
-  OI oi;
-  TunableNumber scoringPos;
+    public final Swerve swerve;
+    public final Elevator elevator;
+    public final Indexer indexer;
+    public final Intake intake;
+    public final Manipulator manipulator;
+    public final OI oi;
+    public final SendableChooser<Command> scoring;
 
-  public RobotContainer() {
-    // swerve = Swerve.getInstance();
-    // swerve = Swerve.getInstance();
-    oi = XboxOI.getInstance();
-    elevator = Elevator.getInstance();
-    manipulator = Manipulator.getInstance();
-    indexer = Indexer.getInstance();
-    intake = Intake.getInstance();
-    scoringPos = new TunableNumber("Elevator Scoring Position", 0, true);
-    // swerve.setDefaultCommand(new TeleopSwerveDrive(swerve, oi));
-    configureBindings();
-  }
+    private boolean intaking;
 
+    public RobotContainer() {
+        swerve = Swerve.getInstance();
+        oi = XboxOI.getInstance();
+        elevator = frc.robot.subsystems.Elevator.getInstance();
+        manipulator = Manipulator.getInstance();
+        indexer = Indexer.getInstance();
+        intake = Intake.getInstance();
 
-  private void configureBindings() {
-    // SysIdSwerve sys = new SysIdSwerve(swerve);
-    // SysIdSwerve sys = new SysIdSwerve(swerve);
+        scoring = new SendableChooser<>();
+        scoring.addOption("L1", elevatorScore(Constants.Elevator.SETPOINTS.L1));
+        scoring.addOption("L2", elevatorScore(Constants.Elevator.SETPOINTS.L2));
+        scoring.addOption("L3", elevatorScore(Constants.Elevator.SETPOINTS.L3));
+        SmartDashboard.putData(scoring);
 
-    // oi.getDriverButton(XboxController.Button.kA.value).whileTrue(sys.sysIdQuasistatic(Direction.kForward));
-    // oi.getDriverButton(XboxController.Button.kB.value).whileTrue(sys.sysIdDynamic(Direction.kForward));
+        swerve.setDefaultCommand(new TeleopSwerveDrive(swerve, oi));
 
-    oi.getDriverButton(XboxController.Button.kY.value).onTrue(elevator.reZero());
-    oi.getDriverButton(XboxController.Button.kB.value).onTrue(new ParallelCommandGroup(
-      manipulator.run(),
-      indexer.run(),
-      intake.run()
-    )).onFalse(new ParallelCommandGroup(
-      manipulator.stop(),
-      indexer.stop(),
-      intake.stop()
-    ));
+        intaking = false;
+        configureBindings();
+    }
 
-    oi.getDriverButton(XboxController.Button.kX.value).onTrue(new ParallelCommandGroup(
-      intake.reverse()
-    )).onFalse(new ParallelCommandGroup(
-      intake.stop()
-    ));
-    
-    // oi.getDriverButton(XboxController.Button.kX.value).onTrue(elevator.moveDown(0.5)).onFalse(elevator.stop());
-
-  }
-
-  public Command intakeCoral() {
-    Elevator elevator = Elevator.getInstance();
-    Indexer indexer = Indexer.getInstance();
-    Manipulator manipulator = Manipulator.getInstance();
-    return elevator.moveElevator(Constants.Elevator.SETPOINTS.IDLE.getSetpoint(), Constants.Elevator.MAX_ERROR).
-    alongWith(intake.moveWrist(Constants.Intake.SETPOINTS.CORAL_PICKUP.getSetpoint(), Constants.Intake.MAX_ERROR)).
-    andThen(intake.run()).
-    alongWith(indexer.run()).
-    alongWith(manipulator.run()).
-    until(manipulator::coralIn).
-    andThen(manipulator.stop()).
-    alongWith(indexer.stop()).
-    alongWith(intake.stop()).
-    andThen(intake.moveWrist(Constants.Intake.SETPOINTS.IDLE.getSetpoint(), Constants.Intake.MAX_ERROR));
-  }
-
-  public Command scoreGamepiece() {
-      double setpoint;
-      switch ((int) scoringPos.get()) {
-          case 1:
-              setpoint = Constants.Elevator.SETPOINTS.L1.getSetpoint();
-              break;
-          case 2:
-              setpoint = Constants.Elevator.SETPOINTS.L2.getSetpoint();
-              break;
-          case 3:
-              setpoint = Constants.Elevator.SETPOINTS.L3.getSetpoint();
-              break;
-          default:
-              System.out.println("please give an actual scoring location");
-              return intake.stop();  // basically i just want to return a command that does nothing
-      }
-      return elevator.moveElevator(setpoint, Constants.Elevator.MAX_ERROR).
-      andThen(manipulator.run()).until(manipulator::coralIn).andThen(manipulator.stop()).
-      andThen(elevator.moveElevator(Constants.Elevator.SETPOINTS.IDLE.getSetpoint(), Constants.Elevator.MAX_ERROR));
-  }
+    public boolean getIntaking() {
+        return intaking;
+    }
 
 
+    private void configureBindings() {
+        // SysIdSwerve sys = new SysIdSwerve(swerve);
+        // SysIdSwerve sys = new SysIdSwerve(swerve);
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return null;
-  }
+        // oi.getDriverButton(XboxController.Button.kA.value).whileTrue(sys.sysIdQuasistatic(Direction.kForward));
+        // oi.getDriverButton(XboxController.Button.kB.value).whileTrue(sys.sysIdDynamic(Direction.kForward));
+
+        oi.getDriverButton(XboxController.Button.kY.value).onTrue(elevator.reZero());
+
+        oi.getDriverButton(XboxController.Button.kB.value).onTrue(tobleIntake());
+
+        oi.getDriverButton(XboxController.Button.kX.value).onTrue(new ParallelCommandGroup(
+            intake.reverse(),
+            indexer.reverse(),
+            manipulator.reverse()
+        )).onFalse(new ParallelCommandGroup(
+            intake.stop(),
+            indexer.stop(),
+            manipulator.stop()
+        ));
+
+        oi.getDriverButton(XboxController.Button.kA.value).onTrue(new InstantCommand(() -> scoring.getSelected().schedule()));
+    }
+
+    public Command tobleIntake() {
+        return new InstantCommand(() -> {
+            (!intaking ? startIntaking() : stopIntaking()).schedule();
+            intaking = !intaking;
+        });
+    }
+
+    public Command startIntaking() {
+       return new SequentialCommandGroup(
+            intake.moveWrist(Constants.Intake.SETPOINTS.CORAL_PICKUP.getSetpoint(), Constants.Intake.MAX_ERROR),
+            new ParallelCommandGroup(
+                manipulator.run(),
+                indexer.run(),
+                intake.run()
+            )
+        ); 
+    }
+
+    public Command stopIntaking() {
+       return intake.moveWrist(Constants.Intake.SETPOINTS.IDLE.getSetpoint(), Constants.Intake.MAX_ERROR).
+            andThen(
+                new ParallelCommandGroup(
+                    manipulator.stop(),
+                    indexer.stop(),
+                    intake.stop()
+                )
+        );
+    }
+
+    public Command elevatorScore(Constants.Elevator.SETPOINTS setpoint) {
+        return new InstantCommand(() -> System.out.println("Setpoint Moving: " + setpoint.getSetpoint())).
+            andThen(elevator.moveElevator(setpoint.getSetpoint()).
+            andThen(manipulator.run()).
+            andThen(new WaitCommand(1.0)).
+            andThen(manipulator.stop()).
+            andThen(elevator.moveElevator(Constants.Elevator.SETPOINTS.IDLE.getSetpoint()))
+        );
+    }
+
+
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return null;
+    }
 }
