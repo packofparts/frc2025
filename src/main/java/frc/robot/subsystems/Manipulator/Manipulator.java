0338e65671e81a9152981daccf9ff56d97c1a4c0
dgorbatov <dzgorbatov@gmail.com>
2025-02-24 09:57:52 -0800

    add can range

--- src/main/java/frc/robot/subsystems/Manipulator/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator/Manipulator.java
@@ -10,6 +10,10 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import com.ctre.phoenix6.configs.CANrangeConfiguration;
+import com.ctre.phoenix6.hardware.CANrange;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 import frc.robot.util.TalonPivot;
 
@@ -17,14 +21,16 @@ public class Manipulator extends TalonPivot {
   private static Manipulator instance;
   private final TalonFX spin;
   private final DutyCycleOut control;
+    private static CANrange range;
+    private static Manipulator instance;
 
-  public static Manipulator getInstance() {
-      if (instance == null) {
-          instance = new Manipulator();
-      }
+    public static Manipulator getInstance() {
+        if (instance == null) {
+            instance = new Manipulator();
+        }
 
-      return instance;
-  }
+        return instance;
+    }
 
   /** Creates a new BaseManipulator. */
   private Manipulator() {
@@ -61,10 +67,27 @@ public class Manipulator extends TalonPivot {
       spin.set(-1 * Constants.Manipulator.SPEED);
     });
   }
+    /** Creates a new BaseManipulator. */
+    private Manipulator() {
+        super(
+            Constants.Manipulator.PIVOT_MOTOR, 
+            null, 
+            Constants.Manipulator.GEAR_RATIO, 
+            Constants.Manipulator.FF, 
+            Constants.Manipulator.ABSOLUTE_ENCODER, 
+            Constants.Manipulator.TUNNING_MODE,
+            "Manipulator"
+        );
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-    super.periodic();
-  }
+        range = new CANrange(Constants.Manipulator.RANGE_ID, Constants.Ports.CANIVORE_NAME);
+
+        CANrangeConfiguration config = new CANrangeConfiguration();
+        range.getConfigurator().apply(config);
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putBoolean("Blocked", range.getIsDetected().getValue());
+        super.periodic();
+    }
 }
